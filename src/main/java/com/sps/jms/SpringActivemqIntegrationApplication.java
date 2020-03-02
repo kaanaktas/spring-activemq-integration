f35280bc4be614b3f6d12e88926529589c2a9c87
aktaskaan@yahoo.com <aktaskaan@yahoo.com>
2020-03-02 00:25:09 +0000

    first commit

---
+++ src/main/java/com/sps/jms/SpringActivemqIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.sps.jms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringActivemqIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringActivemqIntegrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sps/jms/SpringActivemqIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sps.jms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringActivemqIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
